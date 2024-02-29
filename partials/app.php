cb80c394c5665b06abb284347341a61ec283f4e0
codprez production <admin@codprez.com>
2024-02-29 05:55:38 +0000

    init

---
+++ partials/app.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>the Brew - Made by Codprez</title>
+    <link rel="shortcut icon" href="assets/images/logo/favourite_icon.png">
+
+    <!-- fraimwork - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <!-- icon font - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
+
+    <!-- animation - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+
+    <!-- carousel - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
+
+    <!-- popup - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+
+    <!-- jquery-ui - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.css">
+
+    <!-- custom - css include -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+  </head>
+
+
+  <body>
+
+    <!-- body_wrap - start -->
+    <div class="body_wrap">
+
+      <!-- backtotop - start -->
+      <div class="backtotop">
+        <a href="#" class="scroll">
+          <i class="far fa-arrow-up"></i>
+          <i class="far fa-arrow-up"></i>
+        </a>
+      </div>
+      <!-- backtotop - end -->
+
+      <!-- preloader - start -->
+      <div id="preloader"></div>
+      <!-- preloader - end -->
---
+++ partials/footer.php
@@ -0,0 +1,166 @@
+      <!-- footer_section - start
+      ================================================== -->
+      <footer class="footer_section">
+        <div class="footer_widget_area" style="background-image: url(assets/images/backgrounds/bg_04.png);">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-5 col-md-6 col-sm-7">
+                <div class="footer_subscribe_form text-center wow fadeInUp" data-wow-delay=".1s">
+                  <h2 class="form_title text-uppercase">Coffee Build your Fresh mind</h2>
+                  <form action="#">
+                    <div class="form_item">
+                      <input type="email" name="email" placeholder="Enter your email">
+                      <button class="btn btn_primary text-uppercase" type="submit">Subscribe Now</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div class="row justify-content-lg-between">
+              <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="footer_widget footer_about wow fadeInUp" data-wow-delay=".1s">
+                  <div class="brand_logo">
+                    <a class="brand_link" href="/">
+                      <img src="assets/images/logo/logo_black_1x.png" srcset="assets/images/logo/logo_black_2x.png 2x" alt="logo_not_found">
+                    </a>
+                  </div>
+
+                  <p>
+                    Rorem ipsum dolorconsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniaminventore veritatis et.orem ipsum dolorconsectetur adipisicing elit, sed do eiusmod tempor incididunt aneye.
+                  </p>
+
+                  <ul class="social_links social_icons ul_li">
+                    <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#!"><i class="fab fa-youtube"></i></a></li>
+                    <li><a href="#!"><i class="fab fa-behance"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="footer_widget footer_contact wow fadeInUp" data-wow-delay=".2s">
+                  <h3 class="footer_widget_title text-uppercase">Contact us</h3>
+                  <ul class="ul_li_block">
+                    <li><strong class="text-uppercase">Adress:</strong> P.O 453 Awich Road, Gulu</li>
+                    <li><strong class="text-uppercase">Mail:</strong> info@codprez.com</li>
+                    <li><strong class="text-uppercase">Phone:</strong> +256 772032435</li>
+                    <li><strong class="text-uppercase">Fax id:</strong> +256 772032435</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="footer_widget footer_opening_time wow fadeInUp" data-wow-delay=".3s">
+                  <h3 class="footer_widget_title text-uppercase">Opening Hours</h3>
+                  <ul class="ul_li_block">
+                    <li>
+                      Monday
+                      <span>9:00 - 18:00</span>
+                    </li>
+                    <li>
+                      tuesday
+                      <span>10:00 - 18:00</span>
+                    </li>
+                    <li>
+                      wednestday
+                      <span>11:00 - 18:00</span>
+                    </li>
+                    <li>
+                      Thusday
+                      <span>12:00 - 18:00</span>
+                    </li>
+                    <li>
+                      Friday
+                      <span>14:00 - 18:00</span>
+                    </li>
+                    <li>
+                      saterday
+                      <span>16:00 - 18:00</span>
+                    </li>
+                    <li>
+                      Sunday
+                      <span>closed</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="footer_widget footer_recent_post wow fadeInUp" data-wow-delay=".4s">
+                  <h3 class="footer_widget_title text-uppercase">Recent News</h3>
+
+                  <div class="recent_post">
+                    <a class="item_image" href="#">
+                      <img src="assets/images/recent_post/img_01.png" alt="image_not_found">
+                    </a>
+                    <div class="item_content">
+                      <h4 class="item_title">
+                        <a href="#">Best Smell of Americano Coffee Trins</a>
+                      </h4>
+                      <span class="post_date text-uppercase">December 30 - 2021</span>
+                    </div>
+                  </div>
+
+                  <div class="recent_post">
+                    <a class="item_image" href="#">
+                      <img src="assets/images/recent_post/img_02.png" alt="image_not_found">
+                    </a>
+                    <div class="item_content">
+                      <h4 class="item_title">
+                        <a href="#">Best Smell of Americano Coffee Trins</a>
+                      </h4>
+                      <span class="post_date text-uppercase">December 30 - 2021</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="footer_bottom text-center">
+          <div class="container">
+            <p class="copyright_text mb-0">Copyright@ 2023 Desing by <a href="https://codprez.com">Codprez</a></p>
+          </div>
+        </div>
+      </footer>
+      <!-- footer_section - end
+      ================================================== -->
+
+    </div>
+    <!-- body_wrap - end -->
+
+    <!-- fraimwork - jquery include -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- animation - jquery include -->
+    <script src="assets/js/wow.min.js"></script>
+
+    <!-- carousel - jquery include -->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!-- popup - jquery include -->
+    <script src="assets/js/magnific-popup.min.js"></script>
+
+    <!-- isotope filter - jquery include -->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+
+    <!-- google map - jquery include -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDk2HrmqE4sWSei0XdKGbOMOHN3Mm2Bf-M&amp;ver=2.1.6"></script>
+    <script src="assets/js/gmaps.min.js"></script>
+
+    <!-- jquery-ui - jquery include -->
+    <script src="assets/js/jquery-ui.js"></script>
+
+    <!-- off canvas sidebar - jquery include -->
+    <script src="assets/js/mCustomScrollbar.js"></script>
+
+    <!-- custom - jquery include -->
+    <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
